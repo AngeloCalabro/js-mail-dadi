f02ddf63f10ce465f45d22013556f4f1f1a41753
AngeloCalabro <angelo.1991@hotmail.com>
2022-10-11 20:45:13 +0200

    inserita possibilità di cambiare il numero di facce

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,23 +1,27 @@
 let btn = document.querySelector('button');
+let dadoUno = document.getElementById('dado-uno')
+let dadoDue = document.getElementById('dado-due')
+let numeroFacce = 6;
+
 const play = function () {
-    let dadoUno = document.getElementById('dado-uno')
-    let dadoDue = document.getElementById('dado-due')
+
     for (i = 1; i <= 1; i++) {
-        let casualeUno = Math.random() * 6;
-        let casualeDue = Math.random() * 6;
+
+        let casualeUno = Math.random() * numeroFacce;
+        let casualeDue = Math.random() * numeroFacce;
         let numeroCasualeUno = Math.floor(casualeUno);
         let numeroCasualeDue = Math.floor(casualeDue);
         dadoUno.innerHTML = numeroCasualeUno;
         dadoDue.innerHTML = numeroCasualeDue;
         console.log(numeroCasualeUno, numeroCasualeDue);
 
-        let messaggio = document.querySelector('p');
-        if (numeroCasualeUno = numeroCasualeDue) {
-            messaggio.innerHTML = 'Pareggio!'
+        let msg = document.querySelector('p')
+        if (numeroCasualeUno < numeroCasualeDue) {
+            msg.innerHTML = 'Ha vinto il player 2!'
         } else if (numeroCasualeUno > numeroCasualeDue) {
-            messaggio.innerHTML = 'Ha vinto il player 1!'
-        } else if (numeroCasualeUno < numeroCasualeDue) {
-            messaggio.innerHTML = 'Ha vinto il player 2!'
+            msg.innerHTML = 'Ha vinto il player 1!'
+        } else {
+            msg.innerHTML = 'Pareggio!'
         };
 
     };
