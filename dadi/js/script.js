93a2f50e5cd167f61e5611f906a3f830e9c3a947
AngeloCalabro <angelo.1991@hotmail.com>
2022-10-11 20:03:06 +0200

    messaggio dadi

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,25 +1,26 @@
-document.write("Due dadi con 6 faccie", "<br>");
+let btn = document.querySelector('button');
+const play = function () {
+    let dadoUno = document.getElementById('dado-uno')
+    let dadoDue = document.getElementById('dado-due')
+    for (i = 1; i <= 1; i++) {
+        let casualeUno = Math.random() * 6;
+        let casualeDue = Math.random() * 6;
+        let numeroCasualeUno = Math.floor(casualeUno);
+        let numeroCasualeDue = Math.floor(casualeDue);
+        dadoUno.innerHTML = numeroCasualeUno;
+        dadoDue.innerHTML = numeroCasualeDue;
+        console.log(numeroCasualeUno, numeroCasualeDue);
 
-for (i = 1; i <= 1; i++) {
+        let messaggio = document.querySelector('p');
+        if (numeroCasualeUno = numeroCasualeDue) {
+            messaggio.innerHTML = 'Pareggio!'
+        } else if (numeroCasualeUno > numeroCasualeDue) {
+            messaggio.innerHTML = 'Ha vinto il player 1!'
+        } else if (numeroCasualeUno < numeroCasualeDue) {
+            messaggio.innerHTML = 'Ha vinto il player 2!'
+        };
 
-    casuale = Math.random() * 6;
-
-    troncato = Math.floor(casuale);
-
-    numeroDado = troncato + 1;
-
-    document.write(numeroDado);
+    };
 
 };
-
-for (i = 1; i <= 1; i++) {
-
-    casuale = Math.random() * 6;
-
-    troncato = Math.floor(casuale);
-
-    numeroDado = troncato + 1;
-
-    document.write(numeroDado);
-
-}
+btn.addEventListener('click', play);
